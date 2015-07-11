a24e1a46fae9e8fc770bac605e9ed6cd3f5e1162
Alexei Chekulaev <alexei.chekulaev@blinkreaction.com>
2015-07-11 11:15:11 -0400

    Start migration to angular-material

---
+++ App/bower_components/angular-material/modules/closure/whiteframe/whiteframe.js
@@ -0,0 +1,15 @@
+/*!
+ * Angular Material Design
+ * https://github.com/angular/material
+ * @license MIT
+ * v0.10.1-rc1-master-2d78f2c
+ */
+goog.provide('ng.material.components.whiteframe');
+
+/**
+ * @ngdoc module
+ * @name material.components.whiteframe
+ */
+angular.module('material.components.whiteframe', []);
+
+ng.material.components.whiteframe = angular.module("material.components.whiteframe");
